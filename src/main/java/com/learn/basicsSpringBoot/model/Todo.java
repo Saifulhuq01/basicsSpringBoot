1509eb440c87782e27bce18dcc430885a7855381
saifulhuq <saifulhuq@mslabs.co.in>
2025-12-10 18:21:38 +0530

    validation

--- src/main/java/com/learn/basicsSpringBoot/model/Todo.java
+++ src/main/java/com/learn/basicsSpringBoot/model/Todo.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Data
@@ -12,8 +13,11 @@ public class Todo {
     @Id
     @GeneratedValue
     Long id;
+    @NotNull
     String title;
+    @NotNull
     String description;
+    @NotNull
     Boolean isCompleted;
 
 }
